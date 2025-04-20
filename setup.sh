ea2ddc7f2913ce6f9ca66d033dcd3955ccc41ff8
Kaituo Feng <50584334+tulerfeng@users.noreply.github.com>
2025-04-20 14:29:21 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,5 +11,9 @@ pip install flash-attn --no-build-isolation
 # vLLM support 
 pip install vllm==0.7.2
 
+pip install nltk
+pip install rouge_score
+pip install deepspeed
+
 # fix transformers version
 # pip install git+https://github.com/huggingface/transformers.git@336dc69d63d56f232a183a3e7f52790429b871ef
